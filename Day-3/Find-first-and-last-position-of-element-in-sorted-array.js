98dbbc1e8569b60a1d0044bb1cc0eecec96a4ddd
Krishna . <krishnassss365@gmail.com>
2023-10-10 18:22:16 +0000

    #day3

---
+++ Day-3/Find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,36 @@
+var searchRange = function (nums, target) {
+  let result = [-1, -1];
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      result[0] = mid;
+      right = mid - 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  left = 0;
+  right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      result[1] = mid;
+      left = mid + 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return result;
+};
