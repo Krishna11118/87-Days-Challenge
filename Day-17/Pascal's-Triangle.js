7dd8a8e5345f2a233cc218dc778b5fd185c5a807
Krishna <61353958+Krishna11118@users.noreply.github.com>
2023-10-24 22:17:12 +0530

    #day17

---
+++ Day-17/Pascal's-Triangle.js
@@ -0,0 +1,17 @@
+
+var generate = function (numRows) {
+
+    const f = [[1]];
+    for (let i = 0; i < numRows - 1; ++i) {4
+
+        const g = [1];
+        for (let j = 0; j < f[i].length - 1; ++j) {
+            g.push(f[i][j] + f[i][j + 1]);
+        }
+        g.push(1);
+        f.push(g);
+    }
+
+    return f;
+
+};
