a0fb9c0f8d6e3426ebc9332ded17e31909c5d46c
Krishna <61353958+Krishna11118@users.noreply.github.com>
2023-12-19 00:00:22 +0530

    #day67

---
+++ Day-67/ Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal.java
@@ -0,0 +1,23 @@
+
+class Solution {
+   private Map<Integer, Integer> indexes = new HashMap<>();
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        for (int i = 0; i < inorder.length; ++i) {
+            indexes.put(inorder[i], i);
+        }
+        return dfs(inorder, postorder, 0, 0, inorder.length);
+    }
+
+    private TreeNode dfs(int[] inorder, int[] postorder, int i, int j, int n) {
+        if (n <= 0) {
+            return null;
+        }
+        int v = postorder[j + n - 1];
+        int k = indexes.get(v);
+        TreeNode root = new TreeNode(v);
+        root.left = dfs(inorder, postorder, i, j, k - i);
+        root.right = dfs(inorder, postorder, k + 1, j + k - i, n - k + i - 1);
+        return root;
+    }
+}
