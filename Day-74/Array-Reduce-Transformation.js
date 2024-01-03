d0e20b960209602369575851d224f1b3b221a212
Krishna <61353958+Krishna11118@users.noreply.github.com>
2024-01-04 00:05:12 +0530

    #day74

---
+++ Day-74/Array-Reduce-Transformation.js
@@ -0,0 +1,11 @@
+
+var reduce = function(nums, fn, init) {
+    console.log(init)
+        console.log(fn)
+    let accum = init;
+    for (let i = 0; i < nums.length; ++i) {
+        accum = fn(accum, nums[i]);
+        console.log(accum)
+    }
+    return accum;
+};
